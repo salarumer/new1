cf99766a9b55dec1f78aff3ca8d23f4aa43d9f30
Salar Muhammad Umer <114861875+salarumer@users.noreply.github.com>
2025-03-04 19:44:23 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,16 +6,16 @@ gcloud services enable bigquery.googleapis.com
 gcloud services enable bigquerydatatransfer.googleapis.com
 
 # Copy public dataset
-bq mk --force=true --dataset dataset1
+bq mk --force=true --dataset new1data
 bq mk \
   --transfer_config \
   --data_source=cross_region_copy \
-  --target_dataset=dataset1 \
+  --target_dataset=new1data \
   --display_name='SQL Talk Sample Data' \
   --schedule_end_time="$(date -u -d '5 mins' +%Y-%m-%dT%H:%M:%SZ)" \
   --params='{
       "source_project_id":"mltransit",
-      "source_dataset_id":"dataset1",
+      "source_dataset_id":"new1data",
       "overwrite_destination_table":"true"
       }'
 
